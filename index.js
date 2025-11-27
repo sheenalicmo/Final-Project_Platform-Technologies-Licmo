4f19c9a70fca59df5aaac2cd73dba89cc04fd367
sheenalicmo <slicmo.workmail@gmail.com>
2025-11-27 11:58:41 +0000

    Serve HTML and CSS correctly

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Optional: catch-all route for '/'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Public', 'index.html'));
 });
 
 app.listen(PORT, () => {
